9e7aed904ebf8073e1e70313f2342caa32e5900f
Alex Loyko <alex.loyko96@gmail.com>
2024-11-24 13:10:51 -0500

    adding the route

--- src/main/java/com/bytecoders/emergencyaid/AppConfig.java
+++ src/main/java/com/bytecoders/emergencyaid/AppConfig.java
@@ -41,7 +41,7 @@ public class AppConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> {
-      auth.requestMatchers("/hello", "/login", "/register/user", "/patients/new").permitAll();
+      auth.requestMatchers("/hello", "/login", "/register", "/register/user", "/patients/new").permitAll();
       auth.anyRequest().authenticated();
     }).sessionManagement(
         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
